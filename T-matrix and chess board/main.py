0e8b973fea9838aeb92d39dd1b57929eb4470ad4
Nikita Kulakov <69198006+Zubrailx@users.noreply.github.com>
2021-07-29 13:21:32 +0300

    Delete main.py

--- T-matrix and chess board/main.py
+++
@@ -1,9 +0,0 @@
-from matrix import *
-
-n = int(input())
-matrix = [[0 for _ in range(n)] for _ in range(n)]
-
-for i in range(n):
-    for j in range(i):
-        matrix[i][j] = matrix[n - 1 - i][n - 1 - j] = abs(i - j)
-print_matrix(matrix)
